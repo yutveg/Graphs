0c09904565d140781e19d17752e58edb76cb39fe
yutveg <kodi.0n31l@gmail.com>
2020-05-12 17:58:40 -0700

    completo

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -6,36 +6,38 @@ def earliest_ancestor(ancestors, starting_node):
 
     if not get_parents(starting_node):
         return -1
-
+    # initializing a queue to traverse graph nodes in First-In-First-Out order (aka: Breadth-First)
     q = Queue()
-
+    # adding our start / target node to the queue
     q.enqueue(starting_node)
 
-    # visited vertices
     visited = set()
+    # to increment our distance
     distance = 0
-    offset = []
+    added_nodes = [1]
     dist_list = []
     while q.size() > 0:
-        distance += 1
-        
+        if q.size() - added_nodes[distance] == 0:
+            distance += 1
+
         # poppin off the head
         dq = q.dequeue()
-        # if head not visited yet
-
+        # adding node to our list of touched nodes + distance
         dist_list.append((distance, dq))
-        offset = 0
+
         if dq not in visited:
             visited.add(dq)
             # getting parents for current node
+            count = 0
             for parent in get_parents(dq):
                 count += 1
                 q.enqueue(parent)
-
-            offset.append(count)
-
-    dist_list.sort(key=lambda e: e[0], reverse=True)
+            
+            added_nodes.append(count)
+    
+    # sorting stuff and grabbing our lowest id value with highest distance
     print(dist_list)
+    dist_list.sort(key=lambda e: e[0], reverse=True)
     dist_list = [tup[1] for tup in dist_list if tup[0] == dist_list[0][0]]
     
     return min(dist_list)
